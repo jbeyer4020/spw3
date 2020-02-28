cf1ec2d4242b3e33dda675f3cfb8796f84bea77e
Manuel Strasser <S1810307044@students.fh-hagenberg.at>
2020-02-28 17:39:18 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FibonacciSeries {
+  class Program {
+    static void Main(string[] args) {
+      int[] fibonacci = CreateFibonacciSeries(5);
+
+      for (int i = 0; i < fibonacci.Length; i++) {
+        Console.Out.Write(fibonacci[i]);
+        Console.Out.Write(" ");
+      }
+      Console.Out.WriteLine();
+    }
+
+    static int[] CreateFibonacciSeries(int count) {
+      int[] fibonacci = new int[count];
+      fibonacci[0] = 0;
+      fibonacci[1] = 1;
+
+      for (int i = 2; i < count; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+      }
+      return fibonacci;
+    }
+  }
+}
